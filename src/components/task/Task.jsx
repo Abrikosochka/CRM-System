11c1b8404e7b1715b716a2b8e16271c2d2cbb835
Abrikosochka <artemkolesnikov970@gmail.com>
2025-10-14 11:54:39 +0300

    add list tasks to show

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,11 +1,13 @@
 import "./task.css"
 
-const Task = () => {
+const Task = (props) => {
   return (
     <div className="task">
       <div className="task-header">
-        <input type="checkbox" name="complete" className="task_check" />
-        <p>Задача</p>
+        <input type="checkbox" name="complete" className="task_check"
+          checked={props.status}
+        />
+        <p style={{ textDecoration: props.status ? 'line-through' : 'none' }}>{props.text}</p>
       </div>
       <div className="task-buttons">
         <button className="task-buttons_button edit">Редактировать</button>
--- src/components/tasks-list/TaskList.jsx
+++ src/components/tasks-list/TaskList.jsx
@@ -1,13 +1,19 @@
 import "./taskList.css"
 import Task from "../task/Task"
+import { useState } from "react"
 
-const TaskList = () => {
+const TaskList = (props) => {
+  const [tasks, setTasks] = useState([
+    { id: 0, text: 'Задача 1', status: true },
+    { id: 1, text: 'Задача 2', status: true },
+    { id: 2, text: 'Задача 3', status: true },
+    { id: 3, text: 'Задача 4', status: true }
+  ])
   return (
     <div className="tasks-list">
-      <Task></Task>
-      <Task></Task>
-      <Task></Task>
-      <Task></Task>
+      {tasks.map((value) =>
+        <Task key={value.id} text={value.text} status={value.status} />
+      )}
     </div>
   )
 }
